df3d4a40b0dd8ffc7c0833cc9765690685fb37eb
Vincent Rémond <vincent.remond@gmail.com>
2022-06-07 11:10:10 +0200

    Add fork desk

--- SlnLauncher2/Form1.Designer.cs
+++ SlnLauncher2/Form1.Designer.cs
@@ -73,7 +73,7 @@ namespace SlnLauncher2
             this.label1.Location = new System.Drawing.Point(13, 412);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(181, 105);
+            this.label1.Size = new System.Drawing.Size(181, 120);
             this.label1.TabIndex = 4;
             this.label1.Text = resources.GetString("label1.Text");
             this.label1.Click += new System.EventHandler(this.label1_Click);
